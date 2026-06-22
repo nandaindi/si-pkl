fe379a8715d3b82a8f1bc01603af50d3f24e7c9b
kdpras00 <kdpras00@gmail.com>
2026-06-22 18:57:40 +0700

    feat: add site favicon to app and guest layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
     <title>{{ config('app.name', 'SIPKL') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/logosmk.png') }}">
 
     <!-- Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
